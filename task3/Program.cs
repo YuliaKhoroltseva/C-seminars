015dd1fdeb1847aec29daeee2ca379f8f4229891
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-27 16:48:38 +0300

    Перевод числа в день недели

---
+++ task3/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Введите номер дня недели (от 1 до 7): ");
+int numberday = int.Parse(Console.ReadLine());
+
+if(numberday >7)
+{
+    Console.WriteLine("Неверно введен номер дня недели");
+}
+if(numberday <1)
+{
+    Console.WriteLine("Неверно введен номер дня недели");
+}
+if(numberday == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(numberday == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(numberday == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(numberday == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(numberday == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if(numberday == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if(numberday == 7)
+{
+    Console.WriteLine("Воскресенье");
+}

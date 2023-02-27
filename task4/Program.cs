e913e10f72ae2da9b5c01310d072eaef85467068
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-27 16:56:54 +0300

    Все числа от -N до N

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число");
+int number = int.Parse(Console.ReadLine());
+if (number<0)
+{
+    number=number*-1;
+}
+int min = number*-1;
+while (min<number)
+{
+    Console.Write(min);
+    Console.Write(", ");
+    min= min+1;
+}
+Console.Write(number);

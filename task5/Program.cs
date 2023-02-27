35ded9d665310ef683953f2318f1d50880681be0
Khoroltseva.Yulia <yulia.nosovets@mail.ru>
2023-02-27 16:58:15 +0300

    Вывести в терминал последнюю цифру трехзначного числа от пользователя

---
+++ task5/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+if (number<0)
+{
+    number=number*-1;
+}
+if (number < 1000)
+{
+    if (number>99)
+    {
+        int resultnumber = number%10;
+        Console.WriteLine("Последняя цифра ");
+        Console.Write(resultnumber);
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное число, попробуй еще!");
+    }
+    
+}
+ else
+{
+    Console.WriteLine("Введено некорректное число, попробуй еще!");
+}
